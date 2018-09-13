64302e678e075db4a9abaf0dd177dd0441732648
Wai Kit <cwkitkat@gmail.com>
2018-09-13 20:52:15 +0800

    - banyak asset - sum code

---
+++ Assets/Script/Unit/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit {
+
+	protected virtual Vector3 PlayerLocation(){
+		return gameObject.transform.position;
+	}
+}
